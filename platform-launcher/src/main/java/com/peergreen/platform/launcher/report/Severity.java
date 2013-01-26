db5892b5a53437b0b323da7dad767c4e0f60a5c9
Guillaume Sauthier <guillaume.sauthier@peergreen.com>
2013-01-26 10:36:45 +0100

    Platform launcher improvements
    * Micro startup reporting (boot time, number of bundles, errors and warning if any)
    * Supports for bundle StartLevel "encoded" in the resource URL (bundles/(level)/bundle.jar)
    * Supports for persistence (do not re-install or re-start persisted bundles)

---
+++ platform-launcher/src/main/java/com/peergreen/platform/launcher/report/Severity.java
@@ -0,0 +1,12 @@
+package com.peergreen.platform.launcher.report;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guillaume
+ * Date: 23/01/13
+ * Time: 14:53
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Severity {
+    WARNING, ERROR
+}
