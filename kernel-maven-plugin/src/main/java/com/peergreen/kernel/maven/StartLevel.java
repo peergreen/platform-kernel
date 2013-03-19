ffc83d829b0e43edcb9ee2a042c12638becb9730
Florent Benoit <Florent.Benoit@peergreen.com>
2013-03-19 11:22:33 +0100

    Headers

--- kernel-maven-plugin/src/main/java/com/peergreen/kernel/maven/StartLevel.java
+++ kernel-maven-plugin/src/main/java/com/peergreen/kernel/maven/StartLevel.java
@@ -1,18 +1,30 @@
+/**
+ * Copyright 2013 Peergreen S.A.S.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.peergreen.kernel.maven;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA.
- * User: guillaume
- * Date: 24/01/13
- * Time: 22:49
- * To change this template use File | Settings | File Templates.
+ * @author Guillaume Sauthier
  */
 public class StartLevel {
-    private int level = 1;
-    private List<String> bundles = new ArrayList<>();
+    private final int level = 1;
+    private final List<String> bundles = new ArrayList<>();
 
     public int getLevel() {
         return level;
